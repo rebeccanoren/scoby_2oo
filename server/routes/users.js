6ce7500b67f5c9e6c9fe43dc8940b92fa91b4fc2
TheShua <g.deglisezinski@gmail.com>
2020-05-11 18:39:40 +0200

    User API Routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,35 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get('/', (req, res, next) => {
+	const user = req.query.user ? { _id: req.query.user } : null;
+	Item.find(user)
+		.then((dbResult) => res.status(200).json(dbResult))
+		.catch((dbError) => res.status(500).json(dbError));
+});
+
+/**
+ * EDIT ITEM
+ */
+router.patch('/:id', upload.single('image'), (req, res, next) => {
+	let data = req.body;
+	if (req.file) {
+		data.image = req.file.secure_url;
+	}
+	User.findByIdAndUpdate(req.params.id, data, {
+		new: true,
+	})
+		.then((dbResult) => {
+			if (dbResult === null) {
+				res.status(404).json({
+					message: 'User not found',
+				});
+			} else {
+				res.status(200).json(dbResult);
+			}
+		})
+		.catch((dbError) => res.status(500).json(dbError));
 });
 
 module.exports = router;
