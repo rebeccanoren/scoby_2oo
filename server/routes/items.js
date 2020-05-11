f7c1aad8b1d803e148760c63063904588fdc993a
TheShua <g.deglisezinski@gmail.com>
2020-05-11 16:37:08 +0200

    get items route

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,37 +1,59 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 const Item = require('../models/Item');
-const upload = require("../config/cloudinaryConfig");
+const upload = require('../config/cloudinaryConfig');
 
+router.get('/', (req, res, next) => {
+	Item.find()
+		.then((dbResult) => res.status(200).json(dbResult))
+		.catch((dbError) => res.status(500).json(dbError));
+});
 
 /* ADD ITEM */
-router.post("/new", upload.single("image"), (req, res, next) => {
+router.post('/new', upload.single('image'), (req, res, next) => {
+	let data = {
+		id_user: req.session.currentUser._id,
+		name: req.body.name,
+		category: req.body.category,
+		quantity: req.body.quantity,
+		address: req.body.address,
+		description: req.body.description,
+		contact: req.body.contact,
+		location: JSON.parse(req.body.location),
+	};
 
-  let data = {
-    id_user: req.session.currentUser._id,
-    name: req.body.name,
-    category: req.body.category,
-    quantity: req.body.quantity,
-    address: req.body.address,
-    description: req.body.description,
-    contact: req.body.contact,
-    location: JSON.parse(req.body.location)
-  }
+	if (req.file) {
+		let image = req.file.secure_url;
+		data.image = image;
+	}
 
-  if (req.file) {
-    let image = req.file.secure_url;
-    data.image = image
-  }
+	Item.create(data)
+		.then((dbResult) => {
+			res.status(201).json(dbResult);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
+});
 
-  Item.create(
-      data
-    )
-    .then((dbResult) => {
-      res.status(201).json(dbResult);
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
+/**
+ * EDIT ITEM
+ */
+router.patch('/:id', upload.single('image'), (req, res, next) => {
+	let data = req.body;
+	if (req.body.location) data.location = JSON.parse(req.body.location);
+	if (req.file) {
+		data.image = req.file.secure_url;
+	}
+	Item.findByIdAndUpdate(data, req.params.id, { new: true })
+		.then((dbResult) => {
+			if (dbResult === null) {
+				res.status(404).json({ message: 'Item not found' });
+			} else {
+				res.status(200).json(dbResult);
+			}
+		})
+		.catch((dbError) => res.status(500).json(dbError));
 });
 
 module.exports = router;
